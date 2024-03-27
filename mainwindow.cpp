0b1432958c1405d96bcca415c553fd81f5e6b8fc
JohnnySongZ <johnnysongzheng2004@gmail.com>
2024-03-26 18:48:31 -0600

    Added Signal, event handler for buttons and color picking
    The slider, dropdown menu and frame selector do not send signals.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <iostream>
+#include <QColorDialog>
+#include <QColor>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -11,4 +14,66 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::StartProgram(){
+    ui->addFrameButton->setEnabled(true);
+    ui->duplicateFrameButton->setEnabled(true);
+    ui->deleteFrameButton->setEnabled(false);
+    ui->undoButton->setEnabled(false);
+
+}
+
+void MainWindow::on_addFrameButton_clicked(){
+    emit on_addFrameButton_Signal();
+    std::cout << "On add Frame Button signal sent" << std::endl;
+
+}
+
+void MainWindow::on_pencilButton_clicked(){
+    emit on_Pencil_Clicked_Signal();
+    std::cout << "Pencil Clicked signal sent" << std::endl;
+}
+
+void MainWindow::on_eraserButton_clicked(){
+    emit on_Eraser_Clicked_Signal();
+    std::cout << "OnEraser Clicked signal sent" << std::endl;
+}
+
+void MainWindow::on_FPS_clicked(){
+    emit on_FPS_Clicked_Signal();
+    std::cout << "OnFPS Clicked signal sent" << std::endl;
+
+}
+
+void MainWindow::on_SetSize_clicked(){
+    emit on_SetSize_Clicked_Signal();
+    std::cout << "OnSetSize signal sent" << std::endl;
+
+}
+
+void MainWindow::on_actionLoad_clicked(){
+    emit on_Open_Clicked_Signal();
+    std::cout << "OnOpen Clicked signal sent" << std::endl;
+
+}
+
+void MainWindow::on_actionSave_clicked(){
+    emit on_Save_Signal();
+    std::cout << "OnSave Clicked signal sent" << std::endl;
+}
+
+void MainWindow::on_playSpriteButton_clicked(){
+    emit on_PlayBack_Signal();
+    std::cout << "OnPlayBack Clicked signal sent" << std::endl;
+}
+void MainWindow::on_duplicateFrameButton_clicked(){
+    emit on_duplicateFrameButtonClicked_Signal();
+    std::cout << "Duplicate Frame Button Clicked signal sent" << std::endl;
+}
+void MainWindow::on_colorPicker_clicked(){
+
+    QColor selectedColor = QColorDialog::getColor(Qt::white, this, "Select color");
+    emit colorSelected(selectedColor);
+    std::cout <<"Color selected and signal sent" << std::endl;
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,7 +2,6 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <sprite.h>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,6 +16,36 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+signals:
+    void on_Pencil_Clicked_Signal();
+    void on_Eraser_Clicked_Signal();
+    void on_Color_Clicked_Signal();
+    void on_NewFrame_ClickedSignal();
+    void on_FPS_Clicked_Signal();
+    void on_SetSize_Clicked_Signal();
+    void on_Open_Clicked_Signal();
+    void on_Save_Signal();
+    void on_PlayBack_Signal();
+    void on_addFrameButton_Signal();
+    void on_duplicateFrameButtonClicked_Signal();
+    void colorSelected(QColor selectedColor);
+
+
+private slots:
+    void StartProgram();
+    void on_pencilButton_clicked();
+    void on_eraserButton_clicked();
+    void on_FPS_clicked();
+    void on_SetSize_clicked();
+    void on_actionLoad_clicked();
+    void on_actionSave_clicked();
+    void on_playSpriteButton_clicked();
+    void on_addFrameButton_clicked();
+    void on_duplicateFrameButton_clicked();
+    void on_colorPicker_clicked();
+
+
 
 private:
     Ui::MainWindow *ui;
