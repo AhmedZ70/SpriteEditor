96d801b03c7b63ca636e677e432456b1ccbe953f
joecorbeil <josephcorbeil2@gmail.com>
2024-03-27 17:03:42 -0600

    Frame class definitions finished and declarations updated

--- frame.cpp
+++ frame.cpp
@@ -1,9 +1,23 @@
-// #include "frame.h"
+#include "frame.h"
 
-// /**
-//  * @author Joseph Corbeil, Johnny Song, Ezekiel Jaramillo, Ahmed Zahran, Raj Reddy, Joel Ronca
-//  * @date April. 1, 2024
-//  * @name frame cpp file for assignment7
-//  * This cpp file contains the method definitions for a frame to be used in a sprite editor app.
-// */
+/**
+ * @author Joseph Corbeil, Johnny Song, Ezekiel Jaramillo, Ahmed Zahran, Raj Reddy, Joel Ronca
+ * @date April. 1, 2024
+ * @name frame cpp file for assignment7
+ * This cpp file contains the method definitions for a frame to be used in a sprite editor app.
+*/
 
+// Constructor definition
+Frame::Frame(int width, int height) : image(width, height, QImage::Format_ARGB32) {
+    image.fill(Qt::white);
+}
+
+// getImage definition
+QImage& Frame::getImage() { return image; }
+
+// setPixel definition
+void Frame::setPixel(int x, int y, const QColor &color) {
+    if (x >= 0 && x < image.width() && y >= 0 && y < image.height()) {
+        image.setPixelColor(x, y, color);
+    }
+}
